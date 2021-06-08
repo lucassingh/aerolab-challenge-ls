cb7b7467bad61172ee81f1c512c297b9f0341ce2
lucas <lucas.singh@bizitglobal.com>
2021-06-07 22:25:59 -0300

    add component list items

--- src/components/product-list-item/ProductListItem.jsx
+++ src/components/product-list-item/ProductListItem.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalContext';
+import coin from '../../assets/icons/coin.svg';
+import cart2 from '../../assets/images/cart2.svg'
 
-function ProductListItem() {
+function ProductListItem({product}) {
+
+    const { user, setShowModal, setSelectedProduct } = useContext(GlobalContext)
+	const hasRequiredCoins = user.points >= product.cost ? true : false
+	const coinsLeft = product.cost - user.points
+
+	const handleClick = (product) => {
+		setSelectedProduct(product)
+		setShowModal(true)
+	}
+
     return (
-        <div>
-            <h5>Product list item</h5>
-        </div>
-    );
+		<div key={product.id} className="productListItem">
+			<div className="cover">
+				<div>
+					<span>{product.cost}</span>
+					<img src={coin} alt="" />
+				</div>
+				{hasRequiredCoins && (
+					<button onClick={() => handleClick(product)}>Redeem now</button>
+				)}
+			</div>
+			<div className="back">
+				<img src={product.img.url} alt={product.name} />
+
+				<hr />
+				<p>{product.category}</p>
+				<h4>{product.name}</h4>
+			</div>
+			<div>
+				{hasRequiredCoins ? (
+					<img
+						className="badge round"
+						src={cart2}
+						alt="cart"
+					/>
+				) : (
+					<div className="badge rectangle">
+						<span>{`Coins left: ${coinsLeft}`}</span>
+						<img src={coin} alt="coin" />
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default ProductListItem;
--- src/components/product-list/ProductList.jsx
+++ src/components/product-list/ProductList.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalContext';
+import ProductListItem from '../product-list-item/ProductListItem';
+import Modal from '../../components/shared/modal/Modal';
+import ProductChange from '../product-change/ProductChange';
 
-function ProductList() {
-    return (
-        <div>
-            <h3>product list</h3>
-        </div>
-    );
+const ProductList = ({ products, page, productsByPage }) => {
+
+    const { showModal, setShowModal, waitingRequest } = useContext(GlobalContext)
+	const firstProductShowing = page * productsByPage - productsByPage
+	const lastProductShowing = page * productsByPage - 1
+
+    return products ? (
+		<>
+			<div className="productlist">
+				{products.map((product, key) => {
+					if (key > firstProductShowing && key < lastProductShowing) {
+						return <ProductListItem product={product} key={key} />
+					}
+					return ''
+				})}
+			</div>
+			{showModal && (
+				<Modal show={showModal} setShow={setShowModal} loading={waitingRequest}>
+					<ProductChange />
+				</Modal>
+			)}
+		</>
+	) : (
+		''
+	)
 }
 
 export default ProductList;
